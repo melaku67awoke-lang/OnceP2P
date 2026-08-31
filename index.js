3fa07f05b87c10d2df6de70c43df407d70363255
melaku67awoke-lang <melaku67awoke@gmail.com>
2026-08-31 03:53:16 +0300

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-// Serve your frontend HTML file directly at the root URL
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
